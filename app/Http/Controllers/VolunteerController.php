93ccb5297ae2bfd36e2363c6e3ec67933a8d14a2
ica nugraheni <icanugraheni1@gmail.com>
2025-06-02 09:09:35 +0700

    memperbaiki form pengaduan dan volunteer

---
+++ app/Http/Controllers/VolunteerController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\volunteer;
+
+class VolunteerController extends Controller
+{
+    public function create()
+{
+    return view('volunteer.create');
+}
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nama' => 'required|string|max:255',
+            'no_telp' => 'required|string|max:20',
+            'alamat' => 'required|string',
+            'status_kesehatan' => 'required|string',
+        ]);
+
+        if ($request->hasFile('foto')) {
+            $foto = $request->file('foto');
+            $fotoName = time() . '_' . $foto->getClientOriginalName();
+            $foto->move(public_path('foto_volunteer'), $fotoName);
+            $validatedData['foto'] = 'foto_volunteer/' . $fotoName;
+        }
+
+        volunteer::create($validatedData);
+
+        // Arahkan kembali ke halaman utama
+        return redirect('/')->with('success', 'volunteer berhasil dikirim!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PengaduanController;
+use App\Http\Controllers\VolunteerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('pengaduan', PengaduanController::class);
+Route::resource('volunteer', VolunteerController::class);
+
+
+
+
